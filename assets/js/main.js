ee542f322472e7c068fb06b2c34ae1e4aafac7b5
hakikz <hakikzaman@gmail.com>
2021-06-05 16:56:00 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+    $(document).ready(function () {
+    
+    });
+
+    // Popup Youtube
+
+    $('.popup-youtube').magnificPopup({
+        type: 'iframe'
+    });
+
+}(jQuery));
